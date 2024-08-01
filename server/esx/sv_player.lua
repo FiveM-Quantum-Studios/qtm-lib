b888aa553c943098b220d179976f3d12d95662ee
Tom <111147401+1OSaft@users.noreply.github.com>
2024-08-01 14:08:38 +0200

    Add files via upload

--- server/esx/sv_player.lua
+++ server/esx/sv_player.lua
@@ -1,119 +1,87 @@
 if GetResourceState('es_extended') ~= 'started' then return end
 ESX = exports['es_extended']:getSharedObject()
-Framework = 'esx'
-
+Framework = 'ESX'
 
 qtm = {}
-qtm.Player = {}
-
-
-self = {}
-
-
-function qtm.Player(source)
-    local player = ESX.GetPlayerFromId(source)
-
-    local self = {}
-
-    self.job = {
-        name = player.getJob().name,
-        label = player.getJob().label,
-
-        grade = {
-            label = player.getJob().grade_label,
-            level = player.getJob().grade,
-            payment = player.getJob().grade_salary
-        }
-    }
-
-    self.position = player.getCoords(true)
-
-
-
-
-
-
-
-    return self
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function qtm.GetPlayerFromId(source)
-    return qtm.Player[tonumber(source)]
+    local player, self = ESX.GetPlayerFromId(source), {}
+
+    function self.GetJob()
+        return {
+            name = player.getJob().name,
+            label = player.getJob().label,
+
+            grade = {
+                label = player.getJob().grade_label,
+                level = player.getJob().grade,
+                payment = player.getJob().grade_salary
+            }
+        }
+    end
+
+    function self.GetPosition()
+        return player.getCoords(true)
+    end
+
+    function self.GetBank()
+        return player.getAccount('bank').money
+    end
+
+    function self.GetChar()
+        local firstname, lastname = player.getName():match("([^%s]+) ([^%s]+)")
+        return {
+            firstname = firstname,
+            lastname = lastname,
+            gender = player.get('sex'),
+            dateofbirth = player.get('dateofbirth'),
+            phone = player.get('phone_number'),
+        }
+    end
+
+    function self.GetIdentifier()
+        return player.getIdentifier()
+    end
+
+
+
+    -- Create an empty table with a metatable to access player methods
+    local proxy = setmetatable({}, {
+        __index = self
+    })
+
+    return proxy
 end
 
-function qtm.GetPlayerFromId2(source)
-    xPlayer = ESX.GetPlayerFromId(source)
-    return {        --identifier = xPlayer.identifier,
-    accounts = xPlayer.getAccounts(),
-    inventory = xPlayer.getInventory(),
-    job = xPlayer.getJob(),
-    loadout = xPlayer.getLoadout(),
-    money = xPlayer.getMoney(),
-    position = xPlayer.getCoords(true),
-    metadata = xPlayer.getMeta(),}
-end
+exports('getSharedObject', function()
+    return qtm
+end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 RegisterCommand('Testt', function(source)
-    print(ESX.DumpTable(qtm.GetPlayerFromId(source)))
+    qtm2 = exports['qtm-core']:getSharedObject()
+    print(ESX.DumpTable(qtm2.GetPlayerFromId(source).GetChar().dateofbirth))
+    --print(qtm.GetPlayerFromId(source).GetChar().dateofbirth)
 end, false)
-
---[[
-{
-        --identifier = xPlayer.identifier,
-        accounts = xPlayer.getAccounts(),
-        money = xPlayer.getMoney(),
-        position = xPlayer.getCoords(true),
-        metadata = xPlayer.getMeta(),
-    }
-
-
-
-
-
-
-
-PlayerData.job -> Job table of the player
-PlayerData.job.name -> Job name of the player (ex: Police)
-PlayerData.job.label -> Label of the job (ex: Luitenant)
-PlayerData.job.payment -> Job payment amount
-PlayerData.job.onduty -> Duty status of the player
-PlayerData.gang -> Gang table of the player
-PlayerData.gang.name -> Gang name (ex: Ballas)
-PlayerData.gang.label -> Gang label (ex: Boss)
-PlayerData.position -> Current position of the player
-
-PlayerData.money['cash'] -> Total cash on player
-PlayerData.money['bank'] -> Total money on bank
-PlayerData.charinfo -> Character info of player (table)
-PlayerData.charinfo.firstname -> Firstname of the character
-PlayerData.charinfo.lastname -> Lastname of the character
-PlayerData.charinfo.birthdate -> BirthDate of the character
-PlayerData.charinfo.gender -> Gender of character
-PlayerData.charinfo.nationality -> Nationality of character
-PlayerData.charinfo.phone -> Phone number of character
-PlayerData.charinfo.account -> Bank account number of the character
-]]
--- server/qb/sv_player.lua
+++ server/qb/sv_player.lua
@@ -3,37 +3,53 @@ if GetResourceState('qb_core') ~= 'started' then return end
 QBCore = exports['qb-core']:GetCoreObject()
 Framework = 'qb'
 
-
 qtm = {}
 qtm.Player = {}
 
-
-self = {}
-
-
 function qtm.Player(source)
-    local player = QBCore.Functions.GetPlayer(source)
+    local player, self = ESX.GetPlayerFromId(source), {}
 
-    local self = {}
-
-    self.job = {
-        name = player.PlayerData.job.name or player.PlayerData.gang.name,
-        label = player.PlayerData.job.label or player.PlayerData.gang.label,
-
-        grade = {
-            label = player.PlayerData.job.grade.name or player.PlayerData.gang.grade.name,
-            level = player.PlayerData.job.grade.level or player.PlayerData.gang.grade.level,
-            payment = player.PlayerData.job.payment or 0
+    function self.GetJob()
+        return {
+            name = player.PlayerData.job.name or player.PlayerData.gang.name,
+            label = player.PlayerData.job.label or player.PlayerData.gang.label,
+    
+            grade = {
+                label = player.PlayerData.job.grade.name or player.PlayerData.gang.grade.name,
+                level = player.PlayerData.job.grade.level or player.PlayerData.gang.grade.level,
+                payment = player.PlayerData.job.payment or 0
+            }
         }
-    }
+    end
 
-    self.position = player.PlayerData.position
+    function self.GetPosition()
+        return player.PlayerData.position
+    end
+
+    function self.GetBank()
+        return PlayerData.money['bank']
+    end
+
+    function self.GetChar()
+        return {
+            firstname = player.PlayerData.charinfo.firstname,
+            lastname = player.PlayerData.charinfo.lastname,
+            gender = player.PlayerData.charinfo.gender,
+            dateofbirth = player.PlayerData.charinfo.birthdate,
+            phone = player.PlayerData.charinfo.phone,
+        }
+    end
+
+    function self.GetIdentifier()
+        return player.PlayerData.license
+    end
 
 
 
+    -- Create an empty table with a metatable to access player methods
+    local proxy = setmetatable({}, {
+        __index = self
+    })
 
-
-
-
-    return self
+    return proxy
 end
