d3f482f4a3c3423f8d7bdc9e9e6e66f7b7c12aff
Tizas <141794006+Tiziukas@users.noreply.github.com>
2024-08-15 15:17:21 +0100

    qs inventory is pretty shit

--- server/inventory/qs.lua
+++ server/inventory/qs.lua
@@ -1,3 +1,12 @@
+local function table_contains(table, element)
+    for _, value in pairs(table) do
+        if value == element then
+            return true
+        end
+    end
+    return false
+end
+
 Inventory = {
     ---comment: Get player item count
     ---@param source string
@@ -12,8 +21,7 @@ Inventory = {
     ---@param quantity integer
     ---@return boolean | 'qs'
     RemoveItem = function(source, item, quantity, metadata, slot)
-        exports['qs-inventory']:AddItem(source, item, quantity, slot, metadata)
-        return 'qs'
+        return exports['qs-inventory']:RemoveItem(source, item, quantity, slot, metadata)
     end,
     ---comment: Add item to inventory
     ---@param source string
@@ -21,16 +29,36 @@ Inventory = {
     ---@param quantity integer
     ---@return boolean | string | 'qs'
     AddItem = function(source, item, quantity, metadata, slot)
-        exports['qs-inventory']:RemoveItem(source, item, quantity, slot, metadata)
-        return 'qs'
+        return exports['qs-inventory']:AddItem(source, item, quantity, slot, metadata)
     end,
     ---comment: Clear inventory
     ---@param source string
     ---@param keep table
     ---@return boolean | 'qs'
-    ClearInventory = function(source, keep)
-        return 'qs'
+    ClearInventory = function(source, keep) -- Something like this maybe? Need to test.
+        local inventory = exports['qs-inventory']:GetInventory(source)
+        for _, item in pairs(inventory) do
+            if table_contains(keep, item.name) then
+                goto continue
+            else
+                exports['qs-inventory']:RemoveItem(source, item.name, item.count)
+            end
+            ::continue::
+        end
     end,
+
+    ForceOpenInventory = function(source, stashType, stashName)
+        -- ???
+        -- return ox_inventory:forceOpenInventory(source, stashType, stashName)
+    end,
+
+    GetInventory = function(source, owner)
+        return exports['qs-inventory']:GetInventory(source)
+    end,
+
+    RegisterStash = function(id, label, slots, maxWeight, owner, groups, coords, source)
+        return exports['qs-inventory']:RegisterStash(source, id, slots, maxWeight)
+    end
 }
 
 return Inventory
