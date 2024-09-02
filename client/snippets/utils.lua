602cef7bce4051a1602d679f6bb39ee38dc924fe
Tizas <141794006+Tiziukas@users.noreply.github.com>
2024-09-02 16:09:47 +0100

    DrawText3D

--- client/snippets/utils.lua
+++ client/snippets/utils.lua
@@ -26,6 +26,42 @@ Utils = {
 		end
 		return sortedCoords
 	end,
+
+	DrawText3D = function(data)
+		local function GetLineCountAndMaxLength(text)
+			local count = 0
+			local maxLength = 0
+			for line in text:gmatch("([^\n]*)\n?") do
+				count = count + 1
+				local length = #line
+				if length > maxLength then
+					maxLength = length
+				end
+			end
+			return count, maxLength
+		end
+		SetTextScale(0.30, 0.30)
+		SetTextFont(0)
+		SetTextProportional(true)
+		SetTextColour(255, 255, 255, 215)
+		SetTextEntry("STRING")
+		SetTextCentre(true)
+		local totalLength = #data.text
+		local textMaxLength = data.textMaxLength or 99
+		local text = data.text:sub(1, math.min(totalLength, textMaxLength))
+		AddTextComponentString(text)
+		SetDrawOrigin(data.coords.x, data.coords.y, data.coords.z, 0)
+		DrawText(0.0, 0.0)
+		local lineCount, maxLength = GetLineCountAndMaxLength(text)
+		local padding = 0.005
+		local heightFactor = (lineCount / 43) + padding
+		local widthFactor = (maxLength / 100) + padding
+	
+		local height = (heightFactor / 2) - (padding / 2)
+		local width = (widthFactor / 2) - (padding / 2)
+		DrawRect(0.0, height, width, heightFactor, 0, 0, 0, 150)
+		ClearDrawOrigin()
+	end
 }
 
 return Utils
