55e2ef917b0c893242b9b5540399c8ba84f02f18
Tom <emojis-drahtig0@icloud.com>
2024-09-05 17:21:21 +0200

    Fix: Error

--- imports.lua
+++ imports.lua
@@ -3,11 +3,11 @@ qtm = exports['qtm-lib']:getSharedObject()
 if not IsDuplicityVersion() then -- Only register this event for the client
 
     RegisterNetEvent('qtm:onPlayerLoaded', function()
-        Logging('debug', 'Player loaded')
+        qtm.Logging('debug', 'Player loaded')
     end)
 
     RegisterNetEvent('qtm:onPlayerLogout', function()
-        Logging('debug', 'Player logged out')
+        qtm.Logging('debug', 'Player logged out')
     end)
 end
 
