0f2b1ab522c40e87cce6040caa4d74bb0773a95e
Tizas <141794006+Tiziukas@users.noreply.github.com>
2024-09-09 12:51:33 +0100

    Remove Duplicate

--- shared/notification/qtm.lua
+++ shared/notification/qtm.lua
@@ -5,27 +5,27 @@ local NotificationTypes = {
     ['warning'] = 'warning',
 }
 
-Notification = {
-    Notification = function(source, title, type, message, position, colour, iconClass)
-        local notitype = nil
-        if lib.table.contains({'success', 'error', 'info', 'warning'}, type) then
-            notitype = NotificationTypes[type]
-        else
-            notitype = 'inform'
-        end
-        if IsDuplicityVersion() then
-            TriggerClientEvent("qtm-lib:client:notification", source, notitype, message, position, colour, iconClass)
-        else
-            SendNUIMessage({
-                type = "notification",
-                message = message,
-                notificationType = notitype,
-                position = position,
-                backgroundColor = colour,
-                iconClass = iconClass
-            })
-        end
+
+Notification = function(source, title, type, message, position, colour, iconClass)
+    local notitype = nil
+    if lib.table.contains({'success', 'error', 'info', 'warning'}, type) then
+        notitype = NotificationTypes[type]
+    else
+        notitype = 'inform'
     end
-}
+    if IsDuplicityVersion() then
+        TriggerClientEvent("qtm-lib:client:notification", source, notitype, message, position, colour, iconClass)
+    else
+        SendNUIMessage({
+            type = "notification",
+            message = message,
+            notificationType = notitype,
+            position = position,
+            backgroundColor = colour,
+            iconClass = iconClass
+        })
+    end
+end
+
 
 return Notification
